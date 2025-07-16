7371e77965a0f2d548f14ec27e3229bdfd2e0407
Eduardo Botelho <eduardo11224b@gmail.com>
2025-07-16 11:32:23 +0100

    feat: setup base security config class and disable default login form

---
+++ src/main/java/com/upsxace/aces_auth_service/config/SecurityConfig.java
@@ -0,0 +1,74 @@
+package com.upsxace.aces_auth_service.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AnyRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final AppConfig appConfig;
+
+    @Bean
+    @Order(1)
+    @Profile("!prod")
+    public SecurityFilterChain developmentSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**")
+                .authorizeHttpRequests(registry -> registry
+                        .requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
+    @Profile("prod")
+    public SecurityFilterChain productionSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.securityMatcher("/*").redirectToHttps(httpsRedirectConfig ->
+                httpsRedirectConfig.requestMatchers(AnyRequestMatcher.INSTANCE)
+        );
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(3)
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(corsConfig -> corsConfig.configurationSource(request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedOrigins(Collections.singletonList(appConfig.getFrontendUrl()));
+            config.setAllowedMethods(Collections.singletonList("*"));
+            config.setAllowCredentials(true);
+            config.setAllowedHeaders(Collections.singletonList("*"));
+            config.setExposedHeaders(List.of("Authorization")); // Expose Authorization header to the browser (Important for JWT tokens)
+            config.setMaxAge(3600L * 24); // tells the browser to remember these configurations for 24h
+            return config;
+        }));
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(registry -> registry
+                .anyRequest().authenticated()
+        );
+
+        return http.build();
+    }
+
+
+}
