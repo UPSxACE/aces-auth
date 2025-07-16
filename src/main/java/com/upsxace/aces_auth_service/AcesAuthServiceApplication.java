8313cd7d2096689eccf8f1bff44100338733c5ab
Eduardo Botelho <eduardo11224b@gmail.com>
2025-07-16 10:37:09 +0100

    initial setup

---
+++ src/main/java/com/upsxace/aces_auth_service/AcesAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.upsxace.aces_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcesAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcesAuthServiceApplication.class, args);
+	}
+
+}
